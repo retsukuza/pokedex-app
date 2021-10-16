8be07353be07c8131a2563704ca118828d9ce4a2
retsukuza <92241276+retsukuza@users.noreply.github.com>
2021-10-16 17:57:12 +1300

    bruh

--- src/App.js
+++ src/App.js
@@ -21,5 +21,11 @@ function App() {
     </div>
   );
 }
+import React from 'react';
 
+const AllPokemon = () => {
+  return <div>I will hold allllllll of the PokemonCard's!!</div>;
+};
+
+export default AllPokemon;
 export default App;
